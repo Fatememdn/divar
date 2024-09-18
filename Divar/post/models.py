ec269f0f7b424a35df90af105898f676b3fe4b55
Fatememdn <mohammadianfateme86@gmail.com>
2024-09-18 19:44:43 +0330

    filters fix

--- Divar/post/models.py
+++ Divar/post/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from userapp.models import CustomUser
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     is_active = models.BooleanField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
 
 
     
-
--- Divar/post/views.py
+++ Divar/post/views.py
@@ -1,19 +1,30 @@
 from rest_framework.generics import CreateAPIView,RetrieveAPIView, DestroyAPIView, ListAPIView
 from .models import Post
 from .serializers import PostSerializers   
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class PostCreateView(CreateAPIView):
+    permission_classes =  [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializers
 
 class PostListView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+    permission_classes =  [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ['price']
+    search_fields = ['category__name']
+    filterset_fields = ['category__name', 'title']
 
 class PostDetailView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+    permission_classes =  [IsAuthenticated]
 
 class PostDeleteView(DestroyAPIView):
+    permission_classes =  [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializers
